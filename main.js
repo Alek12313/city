5edd76e6223d3b8b90d8c29487238de042c59843
Alex Shen <alexey.shendyk@gmail.com>
2020-11-27 22:45:13 +0200

    refactoring, optimise ID's flow, add .gitignore

--- main.js
+++ main.js
@@ -1,34 +1,45 @@
-const city = [];
+const crypto = require('crypto');
 
-let crId = -1;
+const city = [];
 
 const r = (m) => Math.floor(Math.random() * Math.floor(m));
 
 const savingThrow = (difficult, val) => difficult < val;
+const name = () => crypto.randomBytes(42).toString('hex');
+const nameShorter = () => city.map(el => ({ ...el, name: el.name.substring(4, 20) }));
+const id = (creature) => city.findIndex(el => el.name === creature.name);
 
 const creatures = (n) => Array.apply(null, { length: n })
-  .map(() => ({ id: ++crId, name: Date.now().toString(16), life: 100}));
+  .map(() => ({ name: name(), hp: 100 }));
 
-const die = (el, yes) => { if (!yes) city[el.id].life = 0 };
-const work = (el, yes) => { if (yes) city[el.id].life -= r(100) };
-const eat = (el, yes) => { if (yes) city[el.id].life += r(20) };
-const grave = (el) => { if (el.life <= 0 && !city[el.id].dead) city[el.id].dead = true };
+const survive = (el, yes) => { if (!yes) city[id(el)].hp = 0 };
+const work = (el, yes) => { if (yes) city[id(el)].hp -= r(100) };
+const eat = (el, yes) => { if (yes) city[id(el)].hp += r(20) };
+
+const bury = (el) => { if (el.hp <= 0 && !city[id(el)].dead) city[id(el)].dead = true };
+const LifeRound = (el) => { if (el.hp > 100 && !city[id(el)].dead) city[id(el)].hp = 100 };
 
 const live = () => {
   city.push(...creatures(r(10)));
 
+  // START OF THE DAY, MAIN WORK
   Promise.all(city.map(el => {
     if (!el.dead) {
       work(el, savingThrow(1, r(3)));
-      die(el, savingThrow(1, r(100)));
+      survive(el, savingThrow(1, r(100)));
       eat(el, savingThrow(1, r(3)));
-      die(el, savingThrow(1, r(50)));
+      survive(el, savingThrow(1, r(80)));
     }
   }));
 
-  city.forEach(el => grave(el));
+  // END OF THE DAY, SUMMARY
+  Promise.all(city.map(el => {
+    bury(el);
+    LifeRound(el);
+  }));
 
-  console.log(city);
+
+  console.table(nameShorter());
   // console.log(city.filter(el => !el.dead));
 }
 
